8a5d3e4ebcd25c53c0e3a1ad01e5a29e1c498ab2
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-03-27 07:21:38 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -49,6 +49,7 @@ class InteractiveRecord
   end
 
   def self.find_by(hash)
-    sql = "SELECT * FROM #{self.table_name} WHERE #{}"
+    sql = "SELECT * FROM #{self.table_name} WHERE #{hash.keys[0]} = '#{hash.values[0]}'"
+    DB[:conn].execute(sql)
   end
 end
