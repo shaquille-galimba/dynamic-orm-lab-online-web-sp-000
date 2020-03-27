cbb98c41de3a8033de13409f26a1c6eb8d134634
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-03-27 07:06:45 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -40,7 +40,7 @@ class InteractiveRecord
   end
 
   def save
-    DB[:conn].execute("INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (?)", values_for_insert)
+    DB[:conn].execute("INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})")
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
 end
