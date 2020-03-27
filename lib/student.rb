90d94fe5ba1d42af64956ab3f2149441476ddd85
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-03-27 06:43:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,5 +3,5 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
-
+  self.column_names.each {|column_name| attr_accessor column_name.to_sym}
 end
