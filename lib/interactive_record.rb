a645d967199e6ab6d10f300699019f705632863e
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-03-27 06:39:20 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -5,4 +5,13 @@ class InteractiveRecord
   def self.table_name
     self.to_s.downcase + "s"
   end
+
+  def self.column_names
+    sql = "PRAGMA table_info('#{table_name}')"
+
+    table_info = DB[:conn].execute(sql)
+    column_names = []
+
+    table_info.each {|column| column_names << column["name"]}
+  end
 end
